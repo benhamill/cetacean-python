89abf161e670fda99497ecc60aac0df239af5a01
Ben Hamill <git-commits@benhamill.com>
2015-02-12 15:08:04 -0600

    Move log description to a docstring.

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 # encoding: utf-8
+"""
+The HAL client that does almost nothing for you.
+
+Cetacean is tightly coupled to Requests, but doesn't actually call it. You set
+up your own Requests client and use it to make requests. You feed Cetacean
+Requests response objects and it helps you figure out if they're HAL documents
+and pull useful data out of them if they are.
+"""
 
 from setuptools import setup
 
@@ -10,14 +18,7 @@ setup(
     url="http://github.com/benhamill/cetacean-python#readme",
     licens="MIT",
     description="The HAL client that does almost nothing for you.",
-    long_description="""
-The HAL client that does almost nothing for you.
-
-Cetacean is tightly coupled to Requests, but doesn't actually call it. You set
-up your own Requests client and use it to make requests. You feed Cetacean
-Requests response objects and it helps you figure out if they're HAL documents
-and pull useful data out of them if they are.
-""",
+    long_description=__doc__,
     py_modules=["cetacean"],
     classifiers=[],
 )
