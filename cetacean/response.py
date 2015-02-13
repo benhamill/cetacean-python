0d83f94544a3738254e6294f5fb8ccb2b036e04f
Ben Hamill <git-commits@benhamill.com>
2015-02-12 23:42:15 -0600

    Refactor code out of __init__.py. Still broken.

--- cetacean/response.py
+++ cetacean/response.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
+import re
+
 class Response(object):
-    pass
+
+    """Represents an HTTP response that is hopefully a HAL document."""
+
+    def __init__(self, response):
+        """Pass it a Requests response object.
+
+        :response: A response object from the Requests library.
+
+        """
+        self._response = response
+        self._hal_regex = re.compile(r"application/hal\+json")
+
+    def is_hal(self):
+        """Test if a response was a HAL document or not.
+        :returns: True or False
+
+        """
+        return self._hal_regex.match(self._response.headers['content-type'])
