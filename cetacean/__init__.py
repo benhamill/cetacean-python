d984f486064972a6e86f02e6baac8cdc4023a7d0
Ben Hamill <git-commits@benhamill.com>
2015-02-12 23:21:13 -0600

    Specs to cover just checking the content-type.

--- cetacean/__init__.py
+++ cetacean/__init__.py
@@ -10,3 +10,28 @@ and pull useful data out of them if they are.
 """
 
 __version__ = "0.0.1"
+
+import re
+
+class Cetacean(object):
+    pass
+
+class Cetacean(object):
+
+    """The HAL client that does almost nothing for you."""
+
+    def __init__(self, response):
+        """Pass it a Requests response object.
+
+        :response: A response object from the Requests library.
+
+        """
+        self._response = response
+        self._hal_regex = re.compile(r"application/hal\+json")
+
+    def is_hal(self):
+        """Test if a response was a HAL document or not.
+        :returns: True or False
+
+        """
+        return self._hal_regex.match(self._response.headers['content-type'])
---
+++ spec/acceptance_spec.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# from .context import cetacean
+from expects import *
+import requests
+import httpretty
+import json
+
+from cetacean import Cetacean
+
+from expects.matchers import Matcher
+
+class be_hal(Matcher):
+    def _match(self, response):
+        return response.is_hal()
+
+with describe("Cetacean"):
+    with before.all:
+        httpretty.enable()
+
+    with before.each:
+        httpretty.reset()
+
+    with after.all:
+        httpretty.disable()
+
+    with context("when fed a valid HAL response"):
+        with before.each:
+            httpretty.register_uri(
+                httpretty.GET,
+                "http://api.example.com",
+                content_type="application/hal+json",
+                body=json.dumps(
+                    {
+                       ' _links': {
+                            'self': { 'href': '/' },
+                        },
+                        'api_ranking': 'the best',
+                        '_embedded': {
+                            'singular': {
+                                '_links': { 'self': { 'href': '/singular' } }
+                            },
+                            'plural': [
+                                { '_links': { 'self': { 'href': '/plural/1' } } },
+                                { '_links': { 'self': { 'href': '/plural/2' } } },
+                            ]
+                        }
+                    }
+                ),
+            )
+
+        with it("knows it's HAL"):
+            subject = Cetacean(requests.get("http://api.example.com"))
+
+            expect(subject).to(be_hal())
+
+    with context("when fed invalid HAL"):
+        with before.each:
+            httpretty.register_uri(
+                httpretty.GET,
+                "http://api.example.com",
+                content_type="application/hal+json",
+                body="<html></html>",
+            )
+
+        with it("thinks it's HAL"):
+            subject = Cetacean(requests.get("http://api.example.com"))
+
+            expect(subject).to(be_hal())
+
+    with context("when fed non-HAL"):
+        with before.each:
+            httpretty.register_uri(
+                httpretty.GET,
+                "http://api.example.com",
+                content_type="text/html",
+                body="<html></html>",
+            )
+
+        with it("knows it isn't HAL"):
+            subject = Cetacean(requests.get("http://api.example.com"))
+
+            expect(subject).not_to(be_hal())
